a79bd3c3e51e3986923d7cf156fc1cb14c50ed4c
EnriqueVictoriano <65976413+EnriqueVictoriano@users.noreply.github.com>
2023-06-13 03:46:06 -0400

    Update window.h

--- stage2/window.h
+++ stage2/window.h
@@ -1,1 +1,17 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+#include "magnetic_sensor.h"
+
+class Window
+{
+private:
+    Window(); // a window without a sensor is also not allowed
+public:
+    Window(MagneticSensor sensor);
+    void changeState();
+private:
+    MagneticSensor magneticSensor;
+};
+
+#endif // WINDOW_H
 
