92137b756a11faab0155af9d441f7a341d670a8e
EnriqueVictoriano <65976413+EnriqueVictoriano@users.noreply.github.com>
2023-06-13 03:39:10 -0400

    Create sensor.cpp

---
+++ stage2/sensor.cpp
@@ -0,0 +1,16 @@
+#include "sensor.h"
+#include <iostream>
+using namespace std;
+Sensor::Sensor(int z, bool close){
+    zone = z;
+    state = close;
+}
+bool Sensor::isClose() const {
+    return state;
+}
+int Sensor::getZone() const {
+    return zone;
+}
+void Sensor::setState(bool isClose) {
+    cout << "Sensor is "<< (isClose?"close.":"open.") << endl;
+}
