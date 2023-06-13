b42056e76fd3d196b32b280b2bdfdb3b1b0a58e7
EnriqueVictoriano <65976413+EnriqueVictoriano@users.noreply.github.com>
2023-06-13 03:54:10 -0400

    Update window.cpp

--- stage2/window.cpp
+++ stage2/window.cpp
@@ -1,1 +1,16 @@
+#include "window.h"
+
+Window::Window()
+{
+
+}
+Window::Window(MagneticSensor sensor):magneticSensor() {
+    magneticSensor = sensor;
+}
+void Window::changeState(){
+    if(magneticSensor.isClose())
+        magneticSensor.setSensorOpen();
+    else
+        magneticSensor.setSensorClose();
+}
 
