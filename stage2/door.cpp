0226beca4d267165f653af3dda9850cdfd4b2182
EnriqueVictoriano <65976413+EnriqueVictoriano@users.noreply.github.com>
2023-06-14 20:03:27 -0400

    Update door.cpp

--- stage2/door.cpp
+++ stage2/door.cpp
@@ -1,19 +1,17 @@
 #include "door.h"
 
-Door::Door(int zone): magneticSensor(zone){
+Door::Door(){
 }
 
 Door::Door(MagneticSensor sensor):magneticSensor() {
     magneticSensor = sensor;
+}
+
+Door::Door(int zone): magneticSensor(zone){
 }
 void Door::changeState() {
         if (magneticSensor.isClose())
             magneticSensor.setSensorOpen();
         else
             magneticSensor.setSensorClose();
-}
-
-MagneticSensor Door::getSensor(){
-    return magneticSensor;
-    
 }
